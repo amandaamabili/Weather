48fa19dd3f63491a4a17a4cbb2c6f60a37705550
amandaamabili <amanda_amabilly.b@hotmail.com>
2021-07-31 00:03:44 -0300

    adcionado arquivo

---
+++ src/App.js
@@ -0,0 +1,81 @@
+import React, {useState, useEffect} from 'react'
+import logo from './logo.svg';
+import './App.css';
+import HelloWorld from './components/HelloWorld'
+import {api }from './services/api'
+import { FaTemperatureHigh, FaWind } from 'react-icons/fa'
+
+
+function App() {
+const [weather,setWeather]= useState(null)
+const city= "São Paulo"
+
+  async function handleGetWeather(){
+    const response = await api.get(city)
+    console.log(response.data)
+    setWeather(response.data)
+  }
+  useEffect(()=> {
+    handleGetWeather()
+  })
+
+
+  return (
+    <div className="App">
+      {/*<h1>{"hello world".toUpperCase()}</h1>
+      <HelloWorld></HelloWorld>*/}
+  {/*<header>
+   <button onClick={handleGetWeather}>Enviar</button>
+    </header>
+  */}
+
+{ weather &&
+  <main>
+{/*<p>  {       JSON.stringify(weather)}   </p>*/}
+
+<h1>{city}</h1>
+
+<section className="current-weather">
+
+  <h2>Current weather</h2>
+
+  <p>{weather.temperature}</p>
+  <p>{weather.description}</p>
+
+
+</section>
+
+<section className="forecast">
+  <h2>Forecast</h2>
+  <ol>
+  {
+    weather.forecast.map(day =>
+      <li>
+     <div>      
+       <FaTemperatureHigh> </FaTemperatureHigh>
+  <p>{day.temperature}</p>
+  </div> 
+  <div>
+    <FaWind></FaWind>
+  <p>{day.wind}</p>
+  </div>
+  
+
+      </li>
+    )
+  }
+</ol>
+    
+</section>
+
+</main>
+    
+}
+    </div>
+  );
+
+
+}
+
+export default App;
+
